352c55f56b0d903761174145dc8b662e470739cc
Artem Marchenko <artem.v.marchenko@gmail.com>
2022-09-01 21:51:02 +0300

    feat(artemvmv): added button

--- artemVMV/base-apparel-coming-soon/mail.js
+++ artemVMV/base-apparel-coming-soon/mail.js
@@ -1,28 +1,30 @@
 
 let inputEl = document.getElementById("email")
 let messageEl = document.getElementById("message");
+let fieldElementStyles = document.querySelector(".input-field").style;
+let messageStyles = document.querySelector(".message").style;
 
-
-function statusDisplay() {
+let statusDisplay = () => {
 
     const isEmailValid = emailValidation(inputEl.value);
- 
+  
     if (isEmailValid) {    
         messageEl.textContent = "Your subscription succesfully registered.";
-        inputEl.value= "";
+        fieldElement.border = "1px solid hsl(0, 36%, 70%)";
+        messageSt.color = "green";
+        inputEl.value = "";
         } else {
         messageEl.textContent = "Email is not valid";
+        messageSt.color = "red";
+        fieldElement.border = "2px solid hsl(0, 93%, 68%)";
         inputEl.value = "";
     }
-    console.log(messageEl.textContent)
 }
 
-function emailValidation(email) {
+let  emailValidation = (email) => {
     const validSymbols = /^([a-z\d\.]+)@([a-z\d]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/gmi;
     let validationStatus = validSymbols.test(email);
     console.log(validationStatus)
     return validationStatus
-
-  
 }
 
